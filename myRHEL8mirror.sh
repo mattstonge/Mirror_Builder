65edc36c57e777dbf5264b556888f7ad1e39cdd0
Matt St. Onge <matt_st_onge@yahoo.com>
2023-09-12 16:05:58 -0400

    initial RHEL8 test

--- myRHEL8mirror.sh
+++ myRHEL8mirror.sh
@@ -1,8 +1,45 @@
 # myRHEL8mirror.sh
-# 
-# creates a local mirror of the 3 standard RHEL 8 repos
 #
-# A Red HAt Subscription is required for this to work
+# creates a local mirror of the 3 standard RHEL8 repos
+# Red Hat Subscription is required
 #
-####################################
+# MIT License ( https://opensource.org/license/mit/ )
+#
+##################################
 
+
+##################################
+#
+# Global Variables
+#
+##################################
+
+# set this number to the latest release version number OR the 
+# release version you wish to stop at
+MYRELVER=8.8
+
+# Log entry preface - for easy searches
+LOGGERPREF="[Mirror_Builder] "
+
+
+##################################
+#
+# Functions
+#
+##################################
+
+
+function CheckRegStatus()
+{
+	MYOUTPUT="$LOGGERPREF  Checking on Subscription Status"
+	echo $MYOUTPUT
+	echo $MYOUTPUT | logger
+	SUBSTATUS="$(subscription-manager status | grep Overall )"
+	echo $SUBSTATUS
+	echo "$LOGGERPREF  $SUBSTATUS" | logger
+
+
+}
+
+
+CheckRegStatus
