839e85362855598bf783339aa6af5a3bda102cec
Matt St. Onge <matt_st_onge@yahoo.com>
2023-09-16 17:03:50 -0400

    updated RHEL8 script - still WIP

--- myRHEL8mirror.sh
+++ myRHEL8mirror.sh
@@ -110,9 +110,11 @@ function CronMaster()
 
         echo <<-EOF > /etc/cron.hourly/0reposync
         #!/usr/bin/bash
+
         reposync -p /var/www/html --download-metadata --repoid=rhel-8-for-x86_64-baseos-rpms
         reposync -p /var/www/html --download-metadata --repoid=rhel-8-for-x86_64-baseos-rpms
         reposync -p /var/www/html --download-metadata --repoid=rhel-8-for-x86_64-baseos-rpms
+
         echo "repos synced" | logger
 
         EOF
